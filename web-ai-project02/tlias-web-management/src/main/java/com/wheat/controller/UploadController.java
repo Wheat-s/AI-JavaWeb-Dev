950577c18ef5e5de1dca2831288d55c4911110ce
Wheat-s <wheat8989@gmail.com>
2025-05-23 22:19:49 +0800

    免费试用了阿里云 OSS 存储, 试用了3个月,然后就就不用了,只为学习使用,学完后就把accsesskey 禁用了.... 完成了云端存储的课程.

--- web-ai-project02/tlias-web-management/src/main/java/com/wheat/controller/UploadController.java
+++ web-ai-project02/tlias-web-management/src/main/java/com/wheat/controller/UploadController.java
@@ -1,7 +1,9 @@
 package com.wheat.controller;
 
 import com.wheat.pojo.Result;
+import com.wheat.utils.AliyunOSSOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,8 +15,11 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class UploadController {
-    
-    @PostMapping("/upload")
+
+    /**
+     * 本地磁盘存储方案
+     */
+    /*@PostMapping("/upload")
     public Result upload(String name, String age, MultipartFile file) throws IOException {
         log.info("文件上传中...{},{},{}",  name, age, file);
         // 获取上传的文件名
@@ -25,5 +30,23 @@ public class UploadController {
         // 保存文件
         file.transferTo(new File("/Users/wheat/Downloads/" + newFileName));
         return Result.success();
+    }*/
+    
+    /**
+     * 阿里云OSS存储方案
+     */
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+    
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        // 打印文件信息
+        log.info("文件上传:{}", file.getOriginalFilename());
+        // 将文件交给阿里云OSS服务进行存储
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        // 打印文件存储的url
+        log.info("文件上传成功, url:{}", url);
+        
+        return Result.success(url);
     }
 }
