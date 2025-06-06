5e9c54be2a0749e4130ec5eb370fba8d28cf12d9
Wheat-s <wheat8989@gmail.com>
2025-06-06 22:53:17 +0800

    refactor(dev): 重构开发环境配置和代码
    - 更新 .gitignore 文件，添加更多需要忽略的文件和目录
    - 修改 App.vue 文件，简化结构并添加注释
    - 更新 main.js 文件，移除未使用的导入

--- vue-dev/vue-project1/src/App.vue
+++ vue-dev/vue-project1/src/App.vue
@@ -1,47 +1,15 @@
+<!-- 定义 JS, 控制模板部分的数据和行为(JS) -->
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+//生命一个响应式数据 message
+const message = ref('Hello Vue3');
 </script>
 
+模板部分, 控制的是页面的结构(HTML)
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ message }}</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<!-- 当前组件的 CSS 样式 -->
+<style scoped></style>
