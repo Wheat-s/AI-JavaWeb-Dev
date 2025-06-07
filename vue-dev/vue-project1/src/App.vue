6f35d95f4049aca5970291bb23ad5bb56c3907bc
Wheat-s <wheat8989@gmail.com>
2025-06-07 19:11:33 +0800

    refactor(dev): 重构 App 组件
    - 移除了不必要的脚本代码
    - 引入了 ApiDemo 组件
    - 简化了模板结构
    学习了组合 API的写法.

--- vue-dev/vue-project1/src/App.vue
+++ vue-dev/vue-project1/src/App.vue
@@ -1,14 +1,11 @@
 <!-- 定义 JS, 控制模板部分的数据和行为(JS) -->
 <script setup>
-import { ref } from 'vue';
-
-//生命一个响应式数据 message
-const message = ref('Hello Vue3');
+import ApiDemo from './views/ApiDemo.vue'
 </script>
 
-模板部分, 控制的是页面的结构(HTML)
+<!-- 模板部分, 控制的是页面的结构(HTML) -->
 <template>
-  <h1>{{ message }}</h1>
+  <ApiDemo></ApiDemo>
 </template>
 
 <!-- 当前组件的 CSS 样式 -->
---
+++ vue-dev/vue-project1/src/views/ApiDemo.vue
@@ -0,0 +1,23 @@
+<script setup>
+//引入 ref 函数
+import { onMounted, ref } from 'vue';
+
+//生命响应式数据
+const count = ref(0);
+
+//生命函数 - 在组合api中没有this
+function increment() {
+  count.value++;
+}
+
+//钩子函数
+onMounted(() => {
+  console.log('Vue mounted...');
+})
+</script>
+
+<template>
+  <button @click="increment">count: {{ count }}</button>
+</template>
+
+<style scoped></style>
