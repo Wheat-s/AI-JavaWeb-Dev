62c90640d281083f0b8d83d66f4704e4f23999d7
Wheat-s <wheat8989@gmail.com>
2025-06-08 22:12:48 +0800

    完成了常见组件:表格和分页, 对话框,表单组件,以及案例.

--- vue-dev/vue-project2/src/App.vue
+++ vue-dev/vue-project2/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import ElementDemo from './views/ElementDemo.vue'
+import EmpList from './views/EmpList.vue'
 </script>
 
 <template>
-  <ElementDemo></ElementDemo>
+  <EmpList></EmpList>
 </template>
 
 <style scoped>
