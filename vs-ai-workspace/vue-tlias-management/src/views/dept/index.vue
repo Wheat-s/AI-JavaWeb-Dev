f70651e929b46658074721a3061b9de48127ae09
Wheat-s <wheat8989@gmail.com>
2025-06-12 22:08:40 +0800

    feat(dept): 初步完成部门管理页面搭建
    - 添加部门管理页面的基本布局和样式
    - 实现部门列表的展示，包括序号、部门名称和最后操作时间
    - 添加新增部门按钮和部门列表操作按钮
    - 引入 Element Plus 组件库的表格和按钮组件

--- vs-ai-workspace/vue-tlias-management/src/views/dept/index.vue
+++ vs-ai-workspace/vue-tlias-management/src/views/dept/index.vue
@@ -1,11 +1,194 @@
 <script setup>
+import { ref } from 'vue';
+const tableData = ref([
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  }, {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  }, {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  },
 
+
+
+])
 </script>
 
 <template>
-  部门管理
+  <h1>部门管理</h1>
+  <div class="container">
+    <el-button type="primary">+ 新增部门</el-button>
+  </div>
+  <div class="container">
+    <el-table :data="tableData" border style="width: 100%" height="500">
+      <el-table-column fixed type="index" label="序号" width="100" align="center" />
+      <el-table-column prop="name" label="部门名称" width="260" align="center" />
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center" />
+      <el-table-column label="操作">
+        <template #default="scope">
+          <el-button size="small" type="primary"><el-icon><EditPen /></el-icon>编辑</el-button>
+          <el-button size="small" type="danger"><el-icon><Delete /></el-icon>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <style scoped>
-
+.container {
+  margin: 10px 0;
+}
 </style>
