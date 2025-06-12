dd9218a5bc1aa355c620b63bd09954d31424c921
Wheat-s <wheat8989@gmail.com>
2025-06-12 22:37:38 +0800

    feat(dept): 从接口获取部门数据
    - 移除了静态的 tableData 数组
    - 添加了 deptList 数组用于存储从接口获取的数据
    - 在页面加载时调用 search 函数获取数据
    - 优化了数据展示逻辑，现在使用动态获取的部门数据

--- vs-ai-workspace/vue-tlias-management/src/views/dept/index.vue
+++ vs-ai-workspace/vue-tlias-management/src/views/dept/index.vue
@@ -1,170 +1,20 @@
 <script setup>
-import { ref } from 'vue';
-const tableData = ref([
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  }, {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  }, {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
-  {
-    "id": 1,
-    "name": "学工部",
-    "createTime": "2022-09-01T23:06:29",
-    "updateTime": "2022-09-01T23:06:29"
-  },
+import { ref,onMounted } from 'vue';
+import axios from 'axios';
 
+//钩子函数 页面加载完毕后,执行 搜索函数
+onMounted(() => {
+  search();
+})
+// 查询
+const search = async () => {
+   const result = await axios.get('https://m1.apifoxmock.com/m1/6374871-6071167-default/depts?apifoxApiId=308186010');
+   if (result.data.code) { // JS 隐式类型转换  0 - false, 其他数字 - true; '' - false, 其他都是 true; null, undefined -- false
+    deptList.value = result.data.data;
+   }
+}
 
-
-])
+const deptList = ref([]);
 </script>
 
 <template>
@@ -173,7 +23,7 @@ const tableData = ref([
     <el-button type="primary">+ 新增部门</el-button>
   </div>
   <div class="container">
-    <el-table :data="tableData" border style="width: 100%" height="500">
+    <el-table :data="deptList" border style="width: 100%" height="500">
       <el-table-column fixed type="index" label="序号" width="100" align="center" />
       <el-table-column prop="name" label="部门名称" width="260" align="center" />
       <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center" />
