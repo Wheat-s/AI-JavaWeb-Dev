f9652a7db062cbec433d5e88167d4bbfa276f8db
Wheat-s <wheat8989@gmail.com>
2025-06-22 20:45:25 +0800

    feat(clazz): 实现班级管理功能
    - 添加班级列表查询功能，包括分页和搜索
    - 实现班级信息的展示和筛选
    - 新增班级添加功能和相关表单
    - 优化页面布局和样式

--- vs-ai-workspace/vue-tlias-management/src/views/clazz/index.vue
+++ vs-ai-workspace/vue-tlias-management/src/views/clazz/index.vue
@@ -1,7 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
+import { queryPageApi } from '@/api/clazz'
 
 
+// 钩子函数
+onMounted(() => {
+  search();
+})
 // 搜索表单对象
 const searchClazz = ref({
   name: '',
@@ -10,57 +15,85 @@ const searchClazz = ref({
   end: '',
 })
 
+// 使用 watch() 侦听searchEmp的 date 属性
+watch(() => searchClazz.value.date, (newVal, odlVal) => {
+  if (newVal.length == 2) {
+    searchClazz.value.begin = newVal[0];
+    searchClazz.value.end = newVal[1];
+  } else {
+    searchClazz.value.begin = '';
+    searchClazz.value.begin = '';
+  }
+})
+
+// 查询班级列表
+const search = async () => {
+  const result = await queryPageApi(
+    searchClazz.value.name,
+    searchClazz.value.begin,
+    searchClazz.value.end,
+    currentPage.value,
+    pageSize.value,
+  );
+  if (result.code) {
+    total.value = result.data.total;
+    clazzList.value = result.data.rows
+  }
+}
+
+// 清空
+const clear = () => {
+  searchClazz.value = { name: '', date: [], begin: '', end: '' }
+  search();
+}
+// 添加班级
+const addClazz = () => {
+  dialogFormVisible.value = true
+}
 // 班级表格数据
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
+const clazzList = ref([])
 
 // 分页器
-const currentPage4 = ref(1)
-const pageSize4 = ref(10)
+const currentPage = ref(1)
+const pageSize = ref(10)
 const background = ref(false)
 const total = ref(0)
 // 每页展示记录数变化
 const handleSizeChange = (val) => {
-  console.log(`${val} items per page`)
+  search();
 }
 // 页码变化是触发
 const handleCurrentChange = (val) => {
-  console.log(`current page: ${val}`)
+  search();
 }
+// 添加班级对话框显示
+const dialogFormVisible = ref(false)
+const dialogTitle = ref('新增员工')
 
+const form =  ref({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
 </script>
 
 <template>
   <h1>班级管理</h1>
   <!-- 搜索表单 -->
   <div class="container">
+    {{ searchClazz }}
     <el-form :inline="true" :model="searchClazz" class="demo-form-inline">
       <el-form-item label="班级名称">
-        <el-input v-model="searchClazz.user" placeholder="请输入班级名称" clearable />
+        <el-input v-model="searchClazz.name" placeholder="请输入班级名称" clearable />
       </el-form-item>
       <el-form-item label="节课时间">
-        <el-date-picker v-model="value1" type="daterange" range-separator="至" start-placeholder="开始时间"
-          end-placeholder="结束时间" :size="size" />
+        <el-date-picker v-model="searchClazz.date" type="daterange" range-separator="至" start-placeholder="开始时间"
+          end-placeholder="结束时间" value-format="YYYY-MM-DD" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="search">查询</el-button>
@@ -76,16 +109,17 @@ const handleCurrentChange = (val) => {
 
   <!-- 班级表格列表 -->
   <div class="container">
-    <el-table :data="tableData" border style="width: 100%">
+    <el-table :data="clazzList" border style="width: 100%">
       <el-table-column type="index" label="序号" width="55" align="center" />
-      <el-table-column prop="date" label="班级名称" width="240" align="center" />
-      <el-table-column prop="name" label="班级教室" width="100" align="center" />
-      <el-table-column prop="name" label="班主任" width="100" align="center" />
-      <el-table-column prop="name" label="开课时间" width="130" align="center" />
-      <el-table-column prop="name" label="结课时间" width="130" align="center" />
-      <el-table-column prop="name" label="状态" width="100" align="center" />
-      <el-table-column prop="name" label="最后操作时间" width="220" align="center" />
-      <el-table-column prop="address" label="操作" align="center">
+      <el-table-column prop="name" label="班级名称" width="240" align="center" />
+      <el-table-column prop="room" label="班级教室" width="100" align="center" />
+      <el-table-column prop="masterName" label="班主任" width="100" align="center" />
+      <el-table-column prop="beginDate" label="开课时间" width="130" align="center" />
+      <el-table-column prop="endDate" label="结课时间" width="130" align="center" />
+      <el-table-column prop="status" label="状态" width="100" align="center">
+      </el-table-column>
+      <el-table-column prop="updateTime" label="最后操作时间" width="220" align="center" />
+      <el-table-column label="操作" align="center">
         <template #default="scope">
           <el-button type="primary" @click=""><el-icon>
               <EditPen />
@@ -100,9 +134,35 @@ const handleCurrentChange = (val) => {
 
   <!-- 分页条 -->
   <div class="container">
-    <el-pagination v-model:current-page="currentPage4" v-model:page-size="pageSize4"
-      :page-sizes="[5, 10, 20, 30, 40, 50]" :background="background" layout="total, sizes, prev, pager, next, jumper"
-      :total="total" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+    {{ pageSize }} : {{ currentPage }}
+    <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[5, 10, 20, 30, 40, 50]"
+      :background="background" layout="total, sizes, prev, pager, next, jumper" :total="total"
+      @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+  </div>
+
+  <!-- 新增班级/修改班级 -->
+  <div class="container">
+    <el-dialog v-model="dialogFormVisible" :title="dialogTitle" width="500">
+      <el-form :model="form">
+        <el-form-item label="Promotion name" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="Zones" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="Please select a zone">
+            <el-option label="Zone No.1" value="shanghai" />
+            <el-option label="Zone No.2" value="beijing" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">
+            Confirm
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
